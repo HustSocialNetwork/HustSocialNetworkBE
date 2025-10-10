f2e76475d996dccb41454df6bb6d1c6d4a76b4f9
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2025-10-10 21:53:33 +0700

    chore(init): setup Spring Boot project skeleton

---
+++ src/main/java/vn/hust/social/backend/HustSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package vn.hust.social.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustSocialNetworkApplication.class, args);
+    }
+
+}
