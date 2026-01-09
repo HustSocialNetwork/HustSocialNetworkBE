7277d6c6601b322d3e7ac2d7ca50e84db9002861
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2026-01-09 17:03:38 +0700

    fix: cors and minio credentials

--- src/main/java/vn/hust/social/backend/config/CorsConfig.java
+++ src/main/java/vn/hust/social/backend/config/CorsConfig.java
@@ -14,8 +14,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // frontend dev
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://muniversity-a497b.firebaseapp.com/")); // frontend
+                                                                                                                  // dev
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
