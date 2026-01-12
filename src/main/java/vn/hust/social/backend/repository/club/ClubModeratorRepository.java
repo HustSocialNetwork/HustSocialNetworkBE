eefacc4f2e346b6283ebe3a6e89aee7cbe2a6112
johnwalker189 <bang.nq224826@sis.hust.edu.vn>
2026-01-12 15:21:50 +0700

    add: apis for getting followed clubs and managed clubs + fix: prohibit users from choosing the same display name

--- src/main/java/vn/hust/social/backend/repository/club/ClubModeratorRepository.java
+++ src/main/java/vn/hust/social/backend/repository/club/ClubModeratorRepository.java
@@ -3,6 +3,8 @@ package vn.hust.social.backend.repository.club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.hust.social.backend.entity.club.ClubModerator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import vn.hust.social.backend.entity.enums.club.ClubModeratorStatus;
 import vn.hust.social.backend.entity.enums.club.ClubRole;
@@ -16,4 +18,6 @@ public interface ClubModeratorRepository extends JpaRepository<ClubModerator, UU
     Optional<ClubModerator> findByClubIdAndUserId(UUID clubId, UUID userId);
 
     List<ClubModerator> findAllByClubIdAndRoleAndStatus(UUID clubId, ClubRole role, ClubModeratorStatus status);
+
+    Page<ClubModerator> findByUserIdAndStatus(UUID userId, ClubModeratorStatus status, Pageable pageable);
 }
--- src/main/java/vn/hust/social/backend/repository/club/ClubRepository.java
+++ src/main/java/vn/hust/social/backend/repository/club/ClubRepository.java
@@ -4,9 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.hust.social.backend.entity.club.Club;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import vn.hust.social.backend.entity.enums.club.ClubFollowerStatus;
+import vn.hust.social.backend.entity.enums.club.ClubModeratorStatus;
+
 import java.util.UUID;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, UUID> {
     boolean existsByName(String name);
+
+    @Query("SELECT c FROM Club c JOIN ClubFollower cf ON c.id = cf.club.id WHERE cf.user.id = :userId AND cf.status = :status")
+    Page<Club> findFollowedClubs(UUID userId, ClubFollowerStatus status, Pageable pageable);
+
+    @Query("SELECT c FROM Club c JOIN ClubModerator cm ON c.id = cm.club.id WHERE cm.user.id = :userId AND cm.status = :status")
+    Page<Club> findManagedClubs(UUID userId, ClubModeratorStatus status, Pageable pageable);
 }
